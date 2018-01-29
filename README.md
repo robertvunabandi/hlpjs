cac40e81f50ce6f82a785e3cf3d6d8b32a207dbe
Robert Vunabandi <rvunabandi@gmail.com>
2018-01-29 06:12:21 -0500

    push to github

---
+++ README.md
@@ -0,0 +1,168 @@
+# hlpjs
+
+`hlpjs` is a simple NodeJS / Javascript backend validators and asserter module.
+
+## Contributing
+
+More details on this are to be added.
+
+## Documentations & Methods 
+
+**NOTE: More details about each method will be added. This work is under progress.**
+
+NOTE: The documentation below will offer examples. However, the documentation in the code is very much more detailed about what the requirements / outcomes of the methods will be. Using an IDE such as IDEA PHPStorm or WebStorm helps. 
+
+### Normal Methods
+
+For all of the following methods, assume we called `const hlp = require('hlpjs');`:
+
+#### throw
+```javascript
+hlp.throw (message, ErrorInstance = Error)
+```
+#### isNumber
+```javascript
+hlp.isNumber (arg)
+```
+#### isInteger
+```javascript
+hlp.isInteger (arg)
+```
+#### isNull
+```javascript
+hlp.isNull (arg)
+```
+#### isUndefined
+```javascript
+hlp.isUndefined (arg)
+```
+#### isString
+```javascript
+hlp.isString (arg)
+```
+#### isArray
+```javascript
+hlp.isArray (arg)
+```
+#### isPrimitive
+```javascript
+hlp.isPrimitive (el)
+```
+#### Primitive
+```javascript
+hlp.Primitive(el)
+```
+#### hasStringLength
+```javascript
+hlp.hasStringLength (string, exp_len)
+```
+#### extendString
+```javascript
+hlp.extendString (desired_length, str_or_num, addition_character = ' ')
+```
+#### containsNonNumeric
+```javascript
+hlp.containsNonNumeric (string)
+```
+#### convertStringToNumber
+```javascript
+hlp.convertStringToNumber (string)
+```
+#### isPrimitiveElInArray
+```javascript
+hlp.isPrimitiveElInArray (el, array)
+```
+#### arePrimitiveElsEqual
+```javascript
+hlp.arePrimitiveElsEqual (el1, el2)
+```
+
+### Asserters
+
+These method basically throw an error when the condition to be asserted
+is not met. These methods are found under the key `assert`. For example, 
+```javascript
+const hlp = require('hlpjs');
+hlp.assert.elIsInteger('14'); // throws an error
+```
+Most assert methods have 3 arguments while some of a 4 arguments parameters.
+All asserters' last 2 parameters are `message` and `ErrorInstance`. 
+- `message`: The message you want to be printed out in case the assertion fails
+- `ErrorInstance`: The Error instance to be thrown in case the assertion fails.
+    So, you have the flexibility of choosing what type of error to throw with
+    what message. The only catch is that `ErrorInstance.prototype` must be an 
+    instance of JavaScript's `Error` constructor. I.e., the check 
+    `ErrorInstance.prototype instanceof Error` is made prior to throwing this.
+    In case it's not, it will fall back to `Error`. 
+    
+Assuming we called `const hlp = require('hlpjs');`, here are the methods:
+
+#### elIsNumber
+```javascript
+hlp.assert.elIsNumber (el, message = null, ErrorInstance = null)
+```
+#### elIsInteger
+```javascript
+hlp.assert.elIsInteger (el, message = null, ErrorInstance = null)
+```
+#### elIsNull
+```javascript
+hlp.assert.elIsNull (el, message = null, ErrorInstance = null)
+```
+#### elIsNotNull
+```javascript
+hlp.assert.elIsNotNull (el, message = null, ErrorInstance = null)
+```
+#### elIsNotUndefined
+```javascript
+hlp.assert.elIsNotUndefined (el, message = null, ErrorInstance = null)
+```
+#### elIsString
+```javascript
+hlp.assert.elIsString (el, message = null, ErrorInstance = null)
+```
+#### elIsArray
+```javascript
+hlp.assert.elIsArray (el, message = null, ErrorInstance = null)
+```
+#### elIsPrimitive
+```javascript
+hlp.assert.elIsPrimitive(el, message = null, ErrorInstance = null)
+```
+#### stringHasLength
+```javascript
+hlp.assert.stringHasLength (string, exp_len, message = null, ErrorInstance = Error)
+```
+#### strContainsOnlyNumeric
+```javascript
+hlp.assert.strContainsOnlyNumeric (string, message = null, ErrorInstance = Error)
+```
+#### primitivesAreEqual 
+```javascript
+hlp.assert.primitivesAreEqual (prim1, prim2, message = null, ErrorInstance = Error)
+```
+#### hasStringLength
+```javascript
+hlp.assert.hasStringLength (string, exp_len)
+```
+#### extendString
+```javascript
+hlp.assert.extendString (desired_length, str_or_num, addition_character = ' ')
+```
+#### containsNonNumeric
+```javascript
+hlp.assert.containsNonNumeric (str)
+```
+#### convertStringToNumber
+```javascript
+hlp.assert.convertStringToNumber (str)
+```
+#### isPrimitiveElInArray
+```javascript
+hlp.assert.isPrimitiveElInArray (el, array)
+```
+#### arePrimitiveElsEqual
+```javascript
+hlp.assert.arePrimitiveElsEqual(el1, el2)
+```
+
---
+++ test/helpers.test.js
@@ -0,0 +1,266 @@
+/* global describe, it, expect */
+'use strict';
+const hlp = require('../lib/index');
+const sr_true = 'should return true';
+const sr_false = 'should return false';
+const s_throw = 'should throw exceptions';
+describe('helpers', function () {
+
+  /* throw method */
+
+  describe('throw', function () {
+    it(s_throw, function () {
+      expect(() => hlp.throw('wow')).to.throw(Error);
+      expect(() => hlp.throw('wow', TypeError)).to.throw(TypeError);
+      expect(() => hlp.throw('wow', EvalError)).to.throw(EvalError);
+      expect(() => hlp.throw('wow', RangeError)).to.throw(RangeError);
+      expect(() => hlp.throw('wow', SyntaxError)).to.throw(SyntaxError);
+      expect(() => hlp.throw('wow', ReferenceError)).to.throw(ReferenceError);
+      expect(() => hlp.throw('wow', URIError)).to.throw(URIError);
+    });
+  });
+
+  /* base utils */
+
+  describe('isNumber', function () {
+    it(sr_true, function () {
+      expect(hlp.isNumber(1)).to.equal(true);
+      expect(hlp.isNumber(1.5)).to.equal(true);
+      expect(hlp.isNumber(20)).to.equal(true);
+    });
+    it(sr_false, function () {
+      expect(hlp.isNumber(undefined)).to.equal(false);
+      expect(hlp.isNumber(null)).to.equal(false);
+      expect(hlp.isNumber([])).to.equal(false);
+      expect(hlp.isNumber({})).to.equal(false);
+      expect(hlp.isNumber(NaN)).to.equal(false);
+      expect(hlp.isNumber('a')).to.equal(false);
+    });
+  });
+  describe('isInteger', function () {
+    it(sr_true, function () {
+      expect(hlp.isInteger(1)).to.equal(true);
+      expect(hlp.isInteger(20)).to.equal(true);
+    });
+    it(sr_false, function () {
+      expect(hlp.isInteger(undefined)).to.equal(false);
+      expect(hlp.isInteger(null)).to.equal(false);
+      expect(hlp.isInteger([])).to.equal(false);
+      expect(hlp.isInteger({})).to.equal(false);
+      expect(hlp.isInteger(NaN)).to.equal(false);
+      expect(hlp.isInteger('a')).to.equal(false);
+      expect(hlp.isInteger(1.5)).to.equal(false);
+    });
+  });
+  describe('isNull', function () {
+    it(sr_false, function () {
+      expect(hlp.isNull(undefined)).to.equal(false);
+      expect(hlp.isNull("undefined")).to.equal(false);
+      expect(hlp.isNull("1")).to.equal(false);
+      expect(hlp.isNull({})).to.equal(false);
+      expect(hlp.isNull([])).to.equal(false);
+    });
+    it(sr_true, function () {
+      expect(hlp.isNull(null)).to.equal(true);
+      expect(hlp.isNull(( () => null )())).to.equal(true);
+    });
+  });
+  describe('isUndefined', function () {
+    it(sr_false, function () {
+      expect(hlp.isUndefined(null)).to.equal(false);
+      expect(hlp.isUndefined("undefined")).to.equal(false);
+      expect(hlp.isUndefined("1")).to.equal(false);
+      expect(hlp.isUndefined({})).to.equal(false);
+      expect(hlp.isUndefined([])).to.equal(false);
+    });
+    it(sr_true, function () {
+      expect(hlp.isUndefined(undefined)).to.equal(true);
+      expect(hlp.isUndefined(( () => undefined )())).to.equal(true);
+    });
+  });
+
+  describe('isString', function () {
+    it(sr_false, function () {
+      expect(hlp.isString(null)).to.equal(false);
+      expect(hlp.isString(1)).to.equal(false);
+      expect(hlp.isString(1.52)).to.equal(false);
+      expect(hlp.isString({})).to.equal(false);
+      expect(hlp.isString([])).to.equal(false);
+    });
+    it(sr_true, function () {
+      expect(hlp.isString('undefined')).to.equal(true);
+      expect(hlp.isString(( () => 'hi' )())).to.equal(true);
+    });
+  });
+  describe('isArray', function () {
+    it(sr_false, function () {
+      expect(hlp.isArray(null)).to.equal(false);
+      expect(hlp.isArray(1)).to.equal(false);
+      expect(hlp.isArray(1.52)).to.equal(false);
+      expect(hlp.isArray({})).to.equal(false);
+      expect(hlp.isArray({0: 1, 1: 4})).to.equal(false);
+      expect(hlp.isArray('{}')).to.equal(false);
+    });
+    it(sr_true, function () {
+      expect(hlp.isArray([1,3,5])).to.equal(true);
+      expect(hlp.isArray(( () => ['hi'] )())).to.equal(true);
+    });
+  });
+  describe('isPrimitive', function () {
+    it(sr_true, function () {
+      expect(hlp.isPrimitive(null)).to.equal(true);
+      expect(hlp.isPrimitive(undefined)).to.equal(true);
+      expect(hlp.isPrimitive(1)).to.equal(true);
+      expect(hlp.isPrimitive(1.52)).to.equal(true);
+      expect(hlp.isPrimitive("hello")).to.equal(true);
+    });
+    it(sr_false, function () {
+      expect(hlp.isPrimitive({})).to.equal(false);
+      expect(hlp.isPrimitive([])).to.equal(false);
+    });
+  });
+
+  /* string utils */
+
+  describe('hasStringLength', function () {
+    it(s_throw, function () {
+      expect(() => hlp.hasStringLength()).to.throw(Error);
+      expect(() => hlp.hasStringLength(null)).to.throw(Error);
+      expect(() => hlp.hasStringLength(undefined)).to.throw(Error);
+      expect(() => hlp.hasStringLength([])).to.throw(Error);
+      expect(() => hlp.hasStringLength({})).to.throw(Error);
+      expect(() => hlp.hasStringLength('a')).to.throw(Error);
+      expect(() => hlp.hasStringLength('a', null)).to.throw(Error);
+      expect(() => hlp.hasStringLength('a', undefined)).to.throw(Error);
+      expect(() => hlp.hasStringLength('a', [])).to.throw(Error);
+      expect(() => hlp.hasStringLength('a', {})).to.throw(Error);
+      expect(() => hlp.hasStringLength('a', 1.000000001)).to.throw(Error);
+    });
+    it(sr_false, function () {
+      expect(hlp.hasStringLength('adfsdssf', 1)).to.equal(false);
+      expect(hlp.hasStringLength('1ssd', 3)).to.equal(false);
+    });
+    it(sr_true, function () {
+      expect(hlp.hasStringLength('adfsdssf', 8)).to.equal(true);
+      expect(hlp.hasStringLength('1ssd', 4)).to.equal(true);
+    });
+  });
+
+  describe('extendString', function () {
+    it('should work with numbers', function () {
+      expect(hlp.extendString(5, 1, 0)).to.equal('00001');
+      expect(hlp.extendString(5, 11, 1)).to.equal('11111');
+      expect(hlp.extendString(5, 111)).to.equal('  111');
+      expect(hlp.extendString(5, 1111, 6)).to.equal('61111');
+      expect(hlp.extendString(5, 11111)).to.equal('11111');
+      expect(hlp.extendString(4, 11111)).to.equal('11111');
+    });
+    it('should work with strings', function () {
+      expect(hlp.extendString(5, '1', 0)).to.equal('00001');
+      expect(hlp.extendString(5, '11', '1')).to.equal('11111');
+      expect(hlp.extendString(5, '111')).to.equal('  111');
+      expect(hlp.extendString(5, '1111', 6)).to.equal('61111');
+      expect(hlp.extendString(5, '11111')).to.equal('11111');
+      expect(hlp.extendString(4, '11111')).to.equal('11111');
+    });
+    it(s_throw, function () {
+      expect(() => hlp.extendString('5', '1')).to.throw(Error);
+      expect(() => hlp.extendString(undefined, '1')).to.throw(Error);
+      expect(() => hlp.extendString(null, '1')).to.throw(Error);
+      expect(() => hlp.extendString([], '1')).to.throw(Error);
+      expect(() => hlp.extendString({}, '1')).to.throw(Error);
+      expect(() => hlp.extendString(4, undefined)).to.throw(Error);
+      expect(() => hlp.extendString(4, null)).to.throw(Error);
+      expect(() => hlp.extendString(4, [])).to.throw(Error);
+      expect(() => hlp.extendString(4, {})).to.throw(Error);
+    });
+  });
+
+  describe('containsNonNumeric', function () {
+    it(sr_false, function () {
+      expect(hlp.containsNonNumeric('23532')).to.equal(false);
+      expect(hlp.containsNonNumeric('232')).to.equal(false);
+      expect(hlp.containsNonNumeric('2991')).to.equal(false);
+      expect(hlp.containsNonNumeric('1931')).to.equal(false);
+      expect(hlp.containsNonNumeric('0')).to.equal(false);
+    });
+
+    it(sr_true, function () {
+      expect(hlp.containsNonNumeric('2d3532')).to.equal(true);
+      expect(hlp.containsNonNumeric('2-32')).to.equal(true);
+      expect(hlp.containsNonNumeric('2^991')).to.equal(true);
+      expect(hlp.containsNonNumeric('2`9')).to.equal(true);
+      expect(hlp.containsNonNumeric('193+1')).to.equal(true);
+      expect(hlp.containsNonNumeric('0_')).to.equal(true);
+      expect(hlp.containsNonNumeric('.0')).to.equal(true);
+    });
+
+    it(s_throw, function () {
+      expect(() => hlp.containsNonNumeric(undefined)).to.throw(Error);
+      expect(() => hlp.containsNonNumeric(null)).to.throw(Error);
+      expect(() => hlp.containsNonNumeric(3)).to.throw(Error);
+      expect(() => hlp.containsNonNumeric(1.4)).to.throw(Error);
+      expect(() => hlp.containsNonNumeric([])).to.throw(Error);
+      expect(() => hlp.containsNonNumeric({})).to.throw(Error);
+    });
+  });
+
+  describe('convertStringToNumber', function () {
+    it('should work', function () {
+      expect(hlp.convertStringToNumber('1')).to.equal(1);
+      expect(hlp.convertStringToNumber('13')).to.equal(13);
+      expect(hlp.convertStringToNumber('123413')).to.equal(123413);
+    });
+    it(s_throw, function () {
+      expect(() => hlp.convertStringToNumber(1)).to.throw(Error);
+      expect(() => hlp.convertStringToNumber(455)).to.throw(Error);
+      expect(() => hlp.convertStringToNumber([])).to.throw(Error);
+      expect(() => hlp.convertStringToNumber({})).to.throw(Error);
+      expect(() => hlp.convertStringToNumber(null)).to.throw(Error);
+      expect(() => hlp.convertStringToNumber(undefined)).to.throw(Error);
+    });
+  });
+
+  describe('isElemInArray', function () {
+    it('should throw because of invalid element', function () {
+      expect(() => hlp.isPrimitiveElInArray({}, ['hello'])).to.throw(Error);
+      expect(() => hlp.isPrimitiveElInArray([], ['hello'])).to.throw(Error);
+      expect(() => hlp.isPrimitiveElInArray(() => 3, ['hello'])).to.throw(Error);
+    });
+    it('should throw because of invalid array', function () {
+      expect(() => hlp.isPrimitiveElInArray(1, [{}])).to.throw(Error);
+      expect(() => hlp.isPrimitiveElInArray(1, [[]])).to.throw(Error);
+      expect(() => hlp.isPrimitiveElInArray(1, [() => 1])).to.throw(Error);
+    });
+    it(sr_true, function () {
+      expect(hlp.isPrimitiveElInArray(1, [1, 4, 5, 5, 35, "hello"])).to.equal(true);
+      expect(hlp.isPrimitiveElInArray("1", [1, 4, null, 5, 35, "hello", "1"])).to.equal(true);
+      expect(hlp.isPrimitiveElInArray("hello", [1, 4, 5, null, 35, "hello"])).to.equal(true);
+    });
+    it(sr_false, function () {
+      expect(hlp.isPrimitiveElInArray(2, [1, 4, 5, 5, 35, "hello"])).to.equal(false);
+      expect(hlp.isPrimitiveElInArray("mom", [1, 4, null, 5, 35, "hello", "1"])).to.equal(false);
+      expect(hlp.isPrimitiveElInArray(undefined, [1, 4, 5, null, 35, "hello"])).to.equal(false);
+    });
+  });
+
+  describe('arePrimitiveElsEqual', function () {
+    it(s_throw, function () {
+      expect(() => hlp.arePrimitiveElsEqual([], {})).to.throw(Error);
+      expect(() => hlp.arePrimitiveElsEqual(null, {})).to.throw(Error);
+    });
+    it(sr_false, function () {
+      expect(hlp.arePrimitiveElsEqual(null, 1)).to.equal(false);
+      expect(hlp.arePrimitiveElsEqual(1.4, 1)).to.equal(false);
+      expect(hlp.arePrimitiveElsEqual(1.4, undefined)).to.equal(false);
+    });
+    it(sr_true, function () {
+      expect(hlp.arePrimitiveElsEqual()).to.equal(true);
+      expect(hlp.arePrimitiveElsEqual(null, null)).to.equal(true);
+      expect(hlp.arePrimitiveElsEqual(1, 1)).to.equal(true);
+      expect(hlp.arePrimitiveElsEqual("hello", "hello")).to.equal(true);
+
+    });
+  });
+});
+
